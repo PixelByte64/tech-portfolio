1a3eb554c2da62d1437a277f0ac8b5a3f13211bf
PixelByte64 <40709786+PixelByte64@users.noreply.github.com>
2020-10-26 13:04:47 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Samir Williams', // Title for your website.
-    tagline: 'Programming enthusiast, Aspiaring Music producer.',
+    tagline: 'Programming Enthusiast, Aspiring Music Producer.',
     profilePhoto: 'img/profile.png',
     url: 'https://pixelbyte64.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
